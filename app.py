9ad06d7042eba6a5e1eb00ece9f08110b3be276e
Sam <hello@sammcnally.dev>
2021-09-11 21:53:30 +0100

    add: Post add functionalitly
    posts can now be added to the database

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 import os
+from flask.globals import request
+from flask.helpers import url_for
 import pymongo
+from werkzeug.utils import redirect
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms.widgets import TextArea
+import uuid
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key=os.environ.get("SECRET_KEY")
@@ -10,15 +17,35 @@ client = pymongo.MongoClient(os.environ.get("MONGO_URI"))
 db = client.flaskDB
 
 # Routes
-from auth import routes
+from auth import forms, routes
+from posts import routes
 
 @app.route("/")
 def index():
 	return render_template("index.html")
 
-@app.route("/dashboard/")
+# Register Form Class
+class PostForm(Form):
+    post = TextAreaField('Post', [validators.Length(min=1, max=180)])
+
+@app.route("/dashboard/", methods=['GET', 'POST'])
 def dashboard():
-	return render_template("dashboard.html")
+    posts = db.posts.find()
+    form = PostForm(request.form)
+    if request.method == "POST" and form.validate():
+        print("we're in", form.post.data)
+        post = {
+        "_id": uuid.uuid4().hex,
+        "owner": session["user"],
+        "date": datetime.now(),
+        "post": form.post.data
+        }
+
+        db.posts.insert_one(post)
+        return redirect(url_for('dashboard'))
+    if posts != 0:
+        return render_template("dashboard.html", posts=posts, form=form)
+    return render_template("dashboard.html", form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
