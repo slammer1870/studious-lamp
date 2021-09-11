0bd5c67bf38faf55725f581f507372e83eb014b9
Sam <hello@sammcnally.dev>
2021-09-11 18:11:59 +0100

    add: basic user authentication
    basic user authentication without access controls

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def index():
 
 @app.route("/dashboard")
 def dashboard():
-	return render_template("index.html")
+	return render_template("dashboard.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- auth/routes.py
+++ auth/routes.py
@@ -5,13 +5,27 @@ from flask.helpers import url_for
 from pymongo.common import validate
 from werkzeug.utils import redirect
 from auth.models import User
-from .forms import RegisterForm
+from .forms import RegisterForm, LogInForm
 
 @app.route('/register', methods=['POST', 'GET'])
-def signup():
+def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
         user = User()
         if user.register(form):
             return redirect(url_for('dashboard'))
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LogInForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User()
+        if user.login(form):
+            return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    user = User()
+    return user.logout()
